5673a13510e0079d33a1ac4d2ab122a6f04f5e40
ercanerdogan@gmail.com <ercanerdogan@gmail.com>
2020-11-06 02:22:30 +0300

    UnitOfWork pattern is added

---
+++ NLayerProject.Data/UnitOfWorks/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using NLayerProject.Core.Repositories;
+using NLayerProject.Core.UnitOfWorks;
+using NLayerProject.Data.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerProject.Data.UnitOfWorks
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly AppDbContext _context;
+        private ProductRepository _productRepository;
+        private CategoryRepository _categoryRepository;
+
+        public UnitOfWork(AppDbContext context)
+        {
+            _context = context;
+
+        }
+        public IProductRepository Products => _productRepository = _productRepository ?? new ProductRepository(_context);
+
+        public ICategoryRepository Categories => _categoryRepository = _categoryRepository ?? new CategoryRepository(_context);
+
+        public void Commit()
+        {
+            _context.SaveChanges();
+        }
+
+        public async Task CommitAsync()
+        {
+            await _context.SaveChangesAsync();
+        }
+    }
+}
